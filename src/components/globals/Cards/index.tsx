7a02f661fedc4947900fb94d3cb5c44581130812
Anderson Navarro <andersonaugustonb@ufps.edu.co>
2024-06-18 08:55:47 -0500

    FEATURE: React Query

--- src/components/globals/Cards/index.tsx
+++ src/components/globals/Cards/index.tsx
@@ -4,10 +4,10 @@ import { Card } from 'antd';
 import Image from 'next/image';
 import styles from './styles.module.scss';
 
-const CustomCard: React.FC = ({ icon, text, altIcon='' }) => (
+const CustomCard: React.FC = ({ icon, text, altIcon = '' }) => (
     <Card className={styles['custom-width-card']} >
         <div className={styles.content} >
-            <Image src={icon}  alt={altIcon} />            
+            <Image src={icon} alt={altIcon} className={styles.icon} />
             <p className={styles.text} >{text}</p>
         </div>
     </Card>
